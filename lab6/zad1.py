0579d7c8f8933d3d0b446d2aed7735462813b17a
Sylwester  Macura <1macura@taurus.fis.agh.edu.pl>
2015-05-05 14:14:07 +0200

    Lab 7

---
+++ lab6/zad1.py
@@ -0,0 +1,5 @@
+import numpy as np
+h=200
+Ep=100
+Sp=(Ep)/(2*math.pi*sigmay*sigmaz*u)*np.exp(-y**2.0/(2.0*sigmay**2.0))*np.exp(-H**2.0/(2*sigma**2.0)*1000
+
---
+++ lab7/zad1.py
@@ -0,0 +1,5 @@
+import math
+S=1366.1
+sigma=5.670*10**-8
+A=.3
+T=math.pow(S/(sigma)/4.*(1-A),1./4.)-273.
